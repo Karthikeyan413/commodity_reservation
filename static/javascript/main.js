3dd698d64c86b10f337f183d4f81279bce55edad
Karthikeyan413 <dpmjkarthick413@gmail.com>
2022-04-16 17:29:27 +0530

    home and about skeleton

---
+++ static/javascript/main.js
@@ -0,0 +1,8 @@
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementsByClassName("header")[0].className = "header header-fixed";
+    } else {
+      document.getElementsByClassName("header")[0].className = "header";
+    }
+  }
+  window.onscroll = function() {scrollFunction()};
