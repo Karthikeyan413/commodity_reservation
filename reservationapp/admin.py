46d459e7203039cd23b4bc5af07b7755d74fad1a
Karthikeyan <76156703+Karthikeyan413@users.noreply.github.com>
2022-05-17 10:19:36 +0530

    ticket booking ticket display

--- reservationapp/admin.py
+++ reservationapp/admin.py
@@ -1,9 +1,10 @@
 from time import time
 from django.contrib import admin
-from reservationapp.models import UserCredientials,Ticket,Time,Train,Route
+from reservationapp.models import Commodity, UserCredientials,Ticket,Time,Train,Route
 # Register your models here.
 admin.site.register(UserCredientials)
 admin.site.register(Train)
 admin.site.register(Time)
 admin.site.register(Ticket)
 admin.site.register(Route)
+admin.site.register(Commodity)
